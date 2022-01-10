5871709ba919a548f0be394e4782f9b754d50ce2
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-01-10 19:26:47 +0700

    Update aptinstall_open5gs.sh

--- aptinstall_open5gs.sh
+++ aptinstall_open5gs.sh
@@ -2,11 +2,10 @@
 apt update
 apt install -y net-tools
 apt install -y mongodb
-apt install -y python3-pip python3-setuptools python3-wheel
-apt install -y ninja-build build-essential
-apt install -y flex
-apt install -y bison
-apt install -y libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev 
+apt install -y python3-pip python3-setuptools python3-wheel 
+apt install -y ninja-build build-essential 
+apt install -y flex bison git
+apt inatall -y libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev 
 apt install -y meson
 apt install -y curl
 apt install -y nodejs
