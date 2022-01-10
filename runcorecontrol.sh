44900fe3e5faddbf1825d78ff22c54f3699a3316
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-01-10 21:24:47 +0700

    Add files via upload

---
+++ runcorecontrol.sh
@@ -0,0 +1,17 @@
+# Mongodb
+systemctl start mongodb
+
+# Chang Directory
+cd ~/open5gs
+
+# Network Function
+./install/bin/open5gs-nrfd &
+./install/bin/open5gs-smfd &
+./install/bin/open5gs-amfd &
+./install/bin/open5gs-ausfd &
+./install/bin/open5gs-udmd &
+./install/bin/open5gs-udrd &
+./install/bin/open5gs-pcfd &
+./install/bin/open5gs-nssfd &
+./open5gs/install/bin/open5gs-bsfd
+
