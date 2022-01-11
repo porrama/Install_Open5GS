2a8aaabd6e4fd65acf1e7610201a4edbbfe17ec9
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-01-11 11:07:16 +0700

    Update runnfv_open5gs.sh

--- runnfv_open5gs.sh
+++ runnfv_open5gs.sh
@@ -1,17 +1,14 @@
 # Mongodb
 systemctl start mongodb
 
-# Chang Directory
-cd ~/open5gs/install/bin
-
 # Network Function
-./open5gs-upfd &
-./open5gs-nrfd &
-./open5gs-smfd &
-./open5gs-amfd &
-./open5gs-ausfd &
-./open5gs-udmd &
-./open5gs-udrd &
-./open5gs-pcfd &
-./open5gs-nssfd &
-./open5gs-bsfd 
+~/open5gs/install/bin/open5gs-upfd &
+~/open5gs/install/bin/open5gs-nrfd &
+~/open5gs/install/bin/open5gs-smfd &
+~/open5gs/install/bin/open5gs-amfd &
+~/open5gs/install/bin/open5gs-ausfd &
+~/open5gs/install/bin/open5gs-udmd &
+~/open5gs/install/bin/open5gs-udrd &
+~/open5gs/install/bin/open5gs-pcfd &
+~/open5gs/install/bin/open5gs-nssfd &
+~/open5gs/install/bin/open5gs-bsfd 
